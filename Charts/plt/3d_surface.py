80c6e18bb7cf44b622c79cbab68c09ef4765cc00
GodLovesJonny <zijiewang9928@gmail.com>
2020-01-19 23:37:20 +0800

    plt

--- Charts/plt/3d_surface.py
+++ Charts/plt/3d_surface.py
@@ -10,6 +10,14 @@ X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
 
+# rstride:行之间的跨度  cstride:列之间的跨度
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow')
+
+# 绘制从3D曲面到底部的投影,zdir 可选 'z'|'x'|'y'| 分别表示投影到z,x,y平面
+# zdir = 'z', offset = -2 表示投影到z = -2上
+ax.contour(X, Y, Z, zdir = 'z', offset = -2, cmap = plt.get_cmap('rainbow'))
+
+# 设置z轴的维度，x,y类似
+ax.set_zlim(-2, 2)
 
 plt.show()
